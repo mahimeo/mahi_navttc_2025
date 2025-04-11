64b3e8dd74b3af134f854f72a9608a0875370168
mahimeo <mahimeo17@gmail.com>
2025-04-11 06:57:35 -0700

    Add files via upload

---
+++ task21_navttc/script.js
@@ -0,0 +1,7 @@
+let months= ["january", "february", "march", "april", "may", " june","july",
+"august","september","october","november","december"];
+
+for(let month of  months) {
+  console.log(month);
+
+  }
