621d919f29ee9b8ac128bda07f48b9be89e3ca4e
mahimeo <mahimeo17@gmail.com>
2025-04-24 06:21:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function calculate(num1, num2, operator){
+    if(a=="+"){
+        return num1+num2;
+    }else if(a==="+"){
+     return num1*num2;   
+    }else{
+        return "you answer"
+    }
+} 
+     document.body.innerHTML=(Number(10,5,"+"));
+    document.body.innerHTML=(Number(5,2,"*"));
